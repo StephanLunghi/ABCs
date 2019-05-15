6a512d023f957903f0730eab7f1538488203f8c6
Stephan Lunghi <slunghi43@gmail.com>
2019-05-15 13:33:47 -0400

    Course Project, trying to figure things out.

---
+++ ABCs/Models/Menu.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace ABCs.Models
+{
+    public class Menu
+    {
+        public char Letters { get; set; }
+
+        public string BLOBName { get; set; }
+
+       
+    }
+}
